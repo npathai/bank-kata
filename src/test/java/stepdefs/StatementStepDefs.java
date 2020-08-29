6462f5cebf68918239098ae2781ee67e9e7e9ae8
Narendra Pathai <narendra.pathai@gmail.com>
2020-08-29 00:08:37 -0400

    Added step definition code for date filter in show statement

--- src/test/java/stepdefs/StatementStepDefs.java
+++ src/test/java/stepdefs/StatementStepDefs.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.from;
 
 public class StatementStepDefs {
     private final Fixture application;
@@ -71,6 +72,8 @@ public class StatementStepDefs {
 
     @When("{string} opens account statement filtered by date from {string} to {string}")
     public void opensAccountStatementFilteredByDateFromTo(String accountHolderName, String fromDate, String toDate) {
-
+        application.willReceive(accountHolder.getAccountNoByName(accountHolderName) + " statement --date "
+                + fromDate + " " + toDate);
+        statement = allTransactions(application.readOutput());
     }
 }
